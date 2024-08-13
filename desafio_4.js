26f0f51c8acc1ce3f6b5b638d2cc05da77502c2c
Bianca Cruz <bia34cs@hotmail.com>
2024-08-13 19:46:21 -0300

    Add files via upload

---
+++ desafio_4.js
@@ -0,0 +1,19 @@
+//1.Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+//2.Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let listaLinguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin','Python'];
+//3.Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+listaLinguagensDeProgramacao.push('Java', 'Ruby', 'Swift');
+
+//4.Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+listaDeNomes = ['Bianca', 'Antonio', 'Carlos'];
+console.log(listaDeNomes[0]);
+
+//5.Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+listaDeNomes = ['Bernardo', 'Ana', 'Heitor'];
+console.log(listaDeNomes[1]);
+
+//6.Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+listaDeNomes = ['Aline', 'Amanda', 'João'];
+console.log(listaDeNomes[2]);
